503fce4fe796da777b538345edacdb8c231316cd
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2021-05-22 15:18:39 -0300

    app finish

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -33,9 +33,9 @@ class PlaySoundsViewController: UIViewController {
         case .slow:
             playSound(rate: 0.5)
         case .fast:
-            playSound(rate: 1.5)
+            playSound(rate: 2.0)
         case .chipmunk:
-            playSound(pitch: 1000)
+            playSound(rate: 2.0, pitch: 1000)
         case .vader:
             playSound(pitch: -1000)
         case .echo:
